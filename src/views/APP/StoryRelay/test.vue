0d184e3c2f51c5620db22d10ab8b3cd3a9882a83
1051727403@qq.com <1051727403@qq.com>
2023-06-27 13:03:25 +0800

    2023-6-27 更新

---
+++ src/views/APP/StoryRelay/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <article-comment></article-comment>
+  </div>
+</template>
+
+
+<script>
+import ArticleComment from '@/components/ArticleComment.vue'
+// import Demo from '@/components/Demo.vue'
+
+
+export default {
+  name: 'test',
+  components: {
+    ArticleComment
+  }
+}
+</script>
+
+<style lang="less">
+
+
+
+</style>
