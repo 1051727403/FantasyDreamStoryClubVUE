651104acb7d38aa2f8d53b553120d57dd944bfd9
Ays <2377078993@qq.com>
2023-07-02 22:52:22 +0800

    2023-7-2 搜索功能完善

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav-bar">
-      <el-input class="input-with-select" placeholder="请输入内容" v-model="input1" @keyup.enter="gotowhat" >
+      <el-input class="input-with-select" placeholder="请输入内容" v-model="input1" @keyup.enter.native="gotowhat" >
         <el-button slot="append" icon="el-icon-search"  @click="gotowhat"></el-button>
       </el-input>
       <ul class="the-right" >
@@ -80,7 +80,8 @@ export default {
     },
     gotowhat(){
       console.log("搜索"+this.input1)
-      this.$router.push({ path: '/APP/Search/', query: { keyWords: this.input1 } });
+      this.$router.push({ path: '/APP/Search/', query: { keyWord: this.input1 } });
+      location.reload(0);
     }
   },
 
